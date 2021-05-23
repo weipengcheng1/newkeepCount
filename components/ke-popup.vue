b03091b091524a81f6831ba492c9900cadcbfc7f
75519 <755197142@qq.com>
2021-05-24 01:27:46 +0800

    添加首页及记账弹窗

---
+++ components/ke-popup.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="ke-popup__box">
+		<view class="popup-content__box"><view class="header_box"></view></view>
+	</view>
+</template>
+
+<script>
+export default {
+	name: 'ke-popup',
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.ke-popup__box {
+	width: 100%;
+	height: 100%;
+	position: fixed;
+	bottom: 0;
+	left: 0;
+	background-color: rgba($color: #000000, $alpha: 0.6);
+	.popup-content__box {
+		width: 100%;
+		height: 80%;
+		background-color: #ffffff;
+		border-radius: 20rpx 20rpx 0 0;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+	}
+}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -9,13 +9,12 @@
 			</view>
 			<view class="header-bottom__box">
 				<view class="left-text__box">
-					<text>📅</text>
 					<text>2020年5月</text>
 					<text class="iconfont icon-f11-copy"></text>
 				</view>
 				<view class="right-text__box">
-					<text>😭总支出¥947.17</text>
-					<text>🤩总收入¥947.17</text>
+					<text>总支出¥947.17</text>
+					<text>总收入¥947.17</text>
 				</view>
 			</view>
 		</view>
@@ -24,36 +23,38 @@
 				<view class="card-item_box">
 					<view class="item-header__box">
 						<view class="header-left__box">
-							<text>📅</text>
 							<text>5月14日</text>
 							<text>星期一</text>
 						</view>
 						<view class="header-right__box">
 							<view class="">
-								<text>支</text>
-								<text>0.00</text>
+								<text class="cate_box">支</text>
+								<text class="money_box">0.00</text>
 							</view>
 							<view class="">
-								<text>收</text>
-								<text>0.00</text>
+								<text class="cate_box">收</text>
+								<text class="money_box">0.00</text>
 							</view>
 						</view>
 					</view>
 				</view>
 			</view>
 		</view>
+		<view class="fixed_box">
+			<view class="fixed-cotent_box"><text class="iconfont icon-jinrujizhang"></text></view>
+		</view>
+		<ke-popup></ke-popup>
 	</view>
 </template>
 
 <script>
+import kePopup from '@/components/ke-popup.vue';
 export default {
+	components: {
+		kePopup
+	},
 	data() {
-		return {
-			title: 'Hello',
-			data:{
-				log:"这是新的记录"
-			}
-		};
+		return {};
 	},
 	onLoad() {},
 	methods: {}
@@ -107,7 +108,7 @@ export default {
 		}
 	}
 	.left-text__box {
-		margin-right: 30rpx;
+		margin-right: 60rpx;
 	}
 	.right-text__box {
 		text {
@@ -141,9 +142,68 @@ export default {
 	.item-header__box {
 		width: 100%;
 		box-sizing: border-box;
-		padding: 16rpx 20rpx;
+		padding: 20rpx 30rpx;
 		background-color: #f8f8f8;
-		
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.header-left__box {
+		flex: 0 0 50%;
+		text {
+			&:nth-child(1) {
+				margin-right: 20rpx;
+			}
+		}
+	}
+	.header-right__box {
+		flex: 0 0 50%;
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+		view {
+			&:nth-child(1) {
+				margin-right: 20rpx;
+			}
+		}
+	}
+	.cate_box {
+		display: inline-block;
+		width: 40rpx;
+		height: 40rpx;
+		background-color: #e7e7e7;
+		border-radius: 8rpx;
+		font-size: 24rpx;
+		text-align: center;
+		color: #757575;
+		vertical-align: middle;
+	}
+	.money_box {
+		font-size: 26rpx;
+		font-weight: bold;
+		margin-left: 8rpx;
+	}
+}
+.fixed_box {
+	width: 80rpx;
+	height: 80rpx;
+	border-radius: 50%;
+	background-color: #ffffff;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+	position: fixed;
+	bottom: 15%;
+	right: 40rpx;
+	.fixed-cotent_box {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+	.icon-jinrujizhang {
+		font-size: 40rpx;
+		color: #68a1e8;
+		font-weight: bold;
 	}
 }
 </style>
